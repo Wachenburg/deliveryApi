d6ad9fe0c916418cdeb127494548d5325be93fbf
Lukas Krick <lukaskrick12@gmail.com>
2025-10-27 08:03:58 -0300

    first commit

---
+++ src/main/java/com/deliveryApi/Delivery/API/Project/DeliveryApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryApi.Delivery.API.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryApi/Delivery/API/Project/DeliveryApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryApi.Delivery.API.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
